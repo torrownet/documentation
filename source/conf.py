852869a493f9988aff99fd18c17e8cfa2db903cf
Oksin Aleksandr <oksin.a@torrowtech.com>
2021-08-31 18:29:16 +0300

    background-color - white

--- source/conf.py
+++ source/conf.py
@@ -106,17 +106,18 @@ html_theme = 'renku'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'logo_name': True,
+    'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': "bottom",
     'style_external_links': False,
-    #'style_nav_header_background': 'blue',
+    #'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
     'titles_only': False,
+    #'relbarbgcolor': 'black',
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
